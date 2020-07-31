cc4addaaa706a90fe61ee2d5c708839852e04079
Matheus C. Zaglia <mzaglia@gmail.com>
2020-07-31 16:20:06 -0300

    Update example

--- example/app.py
+++ example/app.py
@@ -2,16 +2,38 @@ import os
 
 from flask import Flask
 from flask_redoc import Redoc
+from marshmallow import Schema, fields
+
+class PetSchema(Schema):
+    name = fields.Str()
 
 app = Flask(__name__)
 
-app.config['REDOC'] = {'title':'Petstore'}
+app.config['REDOC'] = {'title':'Petstore', 'marshmallow_schemas':[PetSchema]}
 
-redoc = Redoc('petstore.yml', app)
+redoc = Redoc(app,'petstore.yml')
+
 
 @app.route('/')
 def index():
     return "Hello World!"
+
+
+@app.route('/random')
+def random():
+    """A cute furry animal endpoint.
+    ---
+    get:
+      description: Get a random pet
+      responses:
+        200:
+          description: Return a pet
+          content:
+            application/json:
+              schema: PetSchema
+    """
+    return PetSchema().dump(dict(name="Bird"))
+
 
 if __name__ == "__main__":
     app.run()
